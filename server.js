8231b506de8007a9ea7111afc66b1c0682717de3
musdy.guner <legend123412@outlook.com>
2022-01-04 17:11:47 +0200

    fix issues

--- server.js
+++ server.js
@@ -19,10 +19,6 @@ connectDB();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
 }
 
 //Cors policy for the communicate with front end / back end
@@ -44,6 +40,10 @@ app.use(
 
 const indexRoute = require("./Route/index");
 app.use("/api/v1/", indexRoute);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT:${PORT}.`);
