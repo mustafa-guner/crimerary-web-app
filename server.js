fef011fe3f861282e27fe1d19d48e0a719044ea8
musdy.guner <legend123412@outlook.com>
2022-01-04 14:14:28 +0200

    fix issues

--- server.js
+++ server.js
@@ -17,11 +17,13 @@ const { CLIENT_URL } = process.env;
 //Connect to database;
 connectDB();
 
+console.log(__dirname);
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
